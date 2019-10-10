73af5775c29f6a4193178cdaf16b67b954d30587
g1d5ny <jiwon936626@naver.com>
2019-10-10 20:34:27 +0900

    여기는 홈페이지

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 # from . import views                           # !!!
 
 urlpatterns = [
-    # path('', views.homepage, name='home'),    # !!!
+    path('', views.homepage, name='home'),    # !!!
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    # path('', include('blog.urls')),
 ]
