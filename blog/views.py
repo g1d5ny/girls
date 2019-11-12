0c90d4257f458c1deca0966f32f1bf4e995d17a4
g1d5ny <jiwon936626@naver.com>
2019-11-12 19:19:10 +0900

    jiwon

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
     # views.post_list 함수는 이제 DB에서 필요한 데이터를 가져와서
@@ -8,7 +10,27 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).\
         order_by('-published_date')
         # 출판일자가 지금보다 이전으로 들어있는 행만 검색
-    return render(       # render() 함수를 호출하여 화면을 출력
+    return render(      # render() 함수를 호출하여 화면을 출력
                 request, # 함수에게 사용자가 요청한 정보를 전달
                 'blog/post_list.html', # 화면 출력 주체 지정
                 {'posts': posts}) # 화면 출력에 사용할 데이터 전달
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+# def post_new(request):
+#     form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
