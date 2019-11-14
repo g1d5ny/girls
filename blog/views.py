f93f4eba6cbfb5bbe36919897fb3ecab6147d2a9
g1d5ny <jiwon936626@naver.com>
2019-11-14 22:06:34 +0900

    Wk11 등록, 수정 폼

--- blog/views.py
+++ blog/views.py
@@ -5,24 +5,39 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 
 def post_list(request):
+
     # views.post_list 함수는 이제 DB에서 필요한 데이터를 가져와서
     # post_list.html에게 넘겨줘야 함
+
     posts = Post.objects.filter(published_date__lte=timezone.now()).\
         order_by('-published_date')
+
         # 출판일자가 지금보다 이전으로 들어있는 행만 검색
     return render(      # render() 함수를 호출하여 화면을 출력
                 request, # 함수에게 사용자가 요청한 정보를 전달
                 'blog/post_list.html', # 화면 출력 주체 지정
                 {'posts': posts}) # 화면 출력에 사용할 데이터 전달
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-# def post_new(request):
-#     form = PostForm()
-#     return render(request, 'blog/post_edit.html', {'form': form})
 def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
